995ccda6e09bc287cb9ad5854310e38fe099fe89
Sam Lindstrom <lindstrom.s@gmail.com>
2015-10-22 16:08:05 -0600

    Added Controller and due_date to model

--- app.js
+++ app.js
@@ -19,30 +19,38 @@ var FormView = Marionette.ItemView.extend({
     'click button': 'createNewTodo'
   },
   ui: {
-    title: 'title'
+    title: 'title',
+    due_date: 'due_date'
   },
   createNewTodo: function(){
     this.collection.add({
-      title: this.ui.title.val()
+      title: this.ui.title.val(),
+      due_date: this.ui.due_date.val()
     });
     this.ui.title.val("");
+    this.ui.due_date.val("");
   }
 });
 
-var AppController = new Marionette.Controller.extend({
+var AppController = Marionette.Controller.extend({
+  showIndex: function(){
+    var TodoView = Marionette.ItemView.extend({
+      template: '#todoView'
+    });
 
-});
-
-TodoTracker.addRegions({
-  form: '#form',
-  list: '#list'
+    TodoTracker.form.show(new FormView({ collection: TodoTracker.todos }));
+    TodoTracker.list.show(new TodosView({ collection: TodoTracker.todos }));
+  }
 });
 
 TodoTracker.addInitializer(function() {
-  TodoTracker.todos = new Todos();
+  TodoTracker.addRegions({
+    form: '#form',
+    list: '#list'
+  });
 
-  TodoTracker.form.show(new FormView({ collection: TodoTracker.todos }));
-  TodoTracker.list.show(new TodosView({ collection: TodoTracker.todos }));
+  TodoTracker.AppController = new AppController();
+  TodoTracker.AppController.showIndex();
 });
 
 TodoTracker.start();
