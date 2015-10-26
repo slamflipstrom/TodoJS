d93f1557723f22ba3f4258405a50fcd92ed1de81
Sam Lindstrom <lindstrom.s@gmail.com>
2015-10-26 13:03:49 -0600

    Fixed Todo title attribute bug and refined styling
    Also adjusted test data to match date picker format.

--- js/todo.model.js
+++ js/todo.model.js
@@ -1,8 +1,8 @@
 var testData = [
-  { id: 1, title: "Vacuum apartment", due_date: "10-24-2015", completed: false},
-  { id: 2, title: "Take cat for a walk", due_date: "10-27-2015", completed: false},
-  { id: 3, title: "Eat entire box of Oreos without guilt", due_date: "10-28-2015", completed: false},
-  { id: 4, title: "Stare lifelessly out window & contemplate the existence of sentient life", due_date: "10-31-2015", completed: false},
+  { id: 1, title: "Vacuum apartment", due_date: "2015-10-24", completed: false},
+  { id: 2, title: "Take cat for a walk", due_date: "2015-10-27", completed: false},
+  { id: 3, title: "Eat entire box of Oreos without guilt", due_date: "2015-10-28", completed: false},
+  { id: 4, title: "Stare lifelessly out window & contemplate the existence of sentient life", due_date: "2015-10-31", completed: false},
 ];
 
 //MODEL//
