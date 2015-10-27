f4a269262c844f0e5c339ebe779115a5d0109cf8
Sam Lindstrom <lindstrom.s@gmail.com>
2015-10-27 01:46:22 -0600

    More Heroku changes

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 var express = require('express'),
  util = require("util");
- 
+
 var app = express();
- 
+
 app.set('view engine', 'jade');
- 
+
 app.get('/', function(req,res){
  res.render('index');
 });
 
 app.use(express.static('js'));
 app.use(express.static('css'));
- 
+
 var port = (process.env.PORT || 3000);
 app.listen(port,null);
